65174ade484105c051569d73152d6fc5ea68bdaf
roger1ms <olegtatur228@mail.ru>
2026-03-15 15:49:02 +0700

    Stack calculator

--- src/lab2/Operations/CalcOperation.java
+++
@@ -1,7 +0,0 @@
-package lab2.Operations;
-import lab2.Context;
-import lab2.exceptions.CommandException;
-
-abstract public class CalcOperation {
-    public abstract void execute(Context context, String[] parsedLine) throws CommandException;
-}
--- src/lab2/Parcer.java
+++
@@ -1,7 +0,0 @@
-package lab2;
-
-public class Parcer {
-    public static String[] parce(String line) {
-        return line.split(" ");
-    }
-}
